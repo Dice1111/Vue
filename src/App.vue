077a87251b33a7a656e4f552fe7b856043d5e0c4
Dice1111 <kingdice1111@gmail.com>
2022-03-09 12:52:32 +0630

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 ref="greet">Hi</h1>
+  <input type="text" ref="name">
+  <button @click="clickHandler">click</button>
+  <div v-if="showModal">
+    <Modal :header="header" :content="content" :theme="theme" @close="showModal=false">
+      <h1 style="color:white;">banana</h1>
+      <p>apple</p>
+    </Modal>
+  </div>
+<!-- ................................Homework.................................... -->
+  <div v-if="showModal2">
+    <Modal :header="header" :content="content" :theme="theme2" @close="showModal2=false">
+      <h1 style="color:white;">Mango</h1>
+      <p>Orange</p>
+    </Modal>
+  </div>
+  <button @click="showModal=true">open modal</button>
+  <button @click="showModal2=true">Delete</button>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Modal from "./components/Modal-Box.vue"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      title:'Happy',
+      header:'Login success',
+      content:'Welcome user apple',
+      theme:'success',
+      theme2:'danger',
+      showModal:false,
+      showModal2:false
+    }
+  },
+  methods:{
+    clickHandler(){
+      this.$refs.name.focus();
+      this.$refs.greet.textContent="Greeting";
+    }
+  },
+  components:{
+    Modal:Modal
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  h1{
+    color: red;
+  }
 </style>
